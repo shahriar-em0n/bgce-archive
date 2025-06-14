5032155311c8988d018df15aebb1f112b95ed72b
nazma98 <nazmasarker1311@gmail.com>
2025-06-14 13:26:08 +0600

    Add code used in class 15 - local scope and block

---
+++ docs/interview-qna/class-wise/golang015-Local-Scope-and-Block/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var (
+	a = 20
+	b = 30
+)
+
+func main() {
+	x := 18
+
+	if x >= 18 {
+		p := 10
+		fmt.Println("I'm matured boy")
+		fmt.Println("I've ", p, " girlfriends")
+		// fmt.Println("I've ", q, " girlfriends")
+
+	}
+
+	// fmt.Println("I've ", p, " girlfriends")
+}
