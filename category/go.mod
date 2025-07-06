678729c47765617c42b7f6c3b5f6a8a29dfe86a2
kraken <javascriptiqbal@gmail.com>
2025-07-06 21:33:55 +0600

    Initialize Category

---
+++ category/go.mod
@@ -0,0 +1,3 @@
+module category
+
+go 1.23.3
---
+++ category/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World with ServeMux!")
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", helloHandler)
+
+	port := ":8080"
+	fmt.Println("Server is running on http://localhost" + port)
+
+	if err := http.ListenAndServe(port, mux); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
