a8d3c78ba32d474b58c3b74c425094114a5b1ffb
TanvirTian <beachpilis@gmail.com>
2025-07-25 15:53:52 +0600

    [product] (feat/create-product): Add createProduct endpoint for adding new products

--- ecommerce/main.go
+++ ecommerce/main.go
@@ -29,7 +29,7 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	if r.Method != "GET" { // r.Method = post, put, patch, delete
-		http.Error(w, "Please Give me GET request", 400)
+		http.Error(w, "Please give me GET request", 400)
 		return 
 	}
 
@@ -38,15 +38,52 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 	encoder.Encode(productList)
 }
 
+func createProduct(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(200)
+		return
+	}
+
+	if r.Method != "POST" { // r.Method = get, put, patch, delete
+		http.Error(w, "Please give me POST request", 400)
+		return 
+	}
+
+	var newProduct Product 
+	
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&newProduct)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Please give me valid json", 400)
+		return 
+	}
+
+	newProduct.ID = len(productList) + 1
+
+	productList = append(productList, newProduct)
+
+	w.WriteHeader(201)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(newProduct)
+}
+
 func main() {
 	mux := http.NewServeMux() // router 
 
 	mux.HandleFunc("/hello", helloHandler) // route
 
-	mux.HandleFunc("/about", aboutHandler) // route
+	mux.HandleFunc("/about", helloHandler) // route
 
 	mux.HandleFunc("/products", getProducts)
 
+	mux.HandleFunc("/create-products", createProduct)
+	
 	fmt.Println("Server running on :8080")
 
 	err := http.ListenAndServe(":8080", mux) //" Failed to start the server"
@@ -80,37 +117,35 @@ func init() {
 		ImgUrl: "https://www.allrecipes.com/thmb/lc7nSL9L5zMHXz9t6PMAVm9biNM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ar-new-banana-adobe-ar-2x1-917fdde58d194b529b41042ebff1c031.jpg",
 	}
 
-	prd4 := Product{
-		ID: 4,
-		Title: "Angur Fol",
-		Description: "Angur Fol Tastes good.",
-		Price: 140,
-		ImgUrl: "https://cdn.dhakapost.com/media/imgAll/BG/2022January/angur-2-20220215152127.jpg",
-	}
+	// prd4 := Product{
+	// 	ID: 4,
+	// 	Title: "Angur Fol",
+	// 	Description: "Angur Fol Tastes good.",
+	// 	Price: 140,
+	// 	ImgUrl: "https://cdn.dhakapost.com/media/imgAll/BG/2022January/angur-2-20220215152127.jpg",
+	// }
 
-	prd5 := Product{
-		ID: 5,
-		Title: "Mango",
-		Description: "Mango is my favorite. I love it very much.",
-		Price: 1000000,
-		ImgUrl: "https://www.dole.com/sites/default/files/styles/512w384h-80/public/media/dole-blog-03-maerz-mango-05.jpg?itok=qXHJMEAz-PEthlz_-",
-	}
+	// prd5 := Product{
+	// 	ID: 5,
+	// 	Title: "Mango",
+	// 	Description: "Mango is my favorite. I love it very much.",
+	// 	Price: 1000000,
+	// 	ImgUrl: "https://www.dole.com/sites/default/files/styles/512w384h-80/public/media/dole-blog-03-maerz-mango-05.jpg?itok=qXHJMEAz-PEthlz_-",
+	// }
 
-	prd6 := Product{
-		ID: 6,
-		Title: "Strawberry",
-		Description: "Strawberries are sweet, juicy, and bursting with flavor.",
-		Price: 500,
-		ImgUrl: "https://snaped.fns.usda.gov/sites/default/files/styles/crop_ratio_7_5/public/seasonal-produce/2018-05/strawberries.jpg.webp?itok=B4LFd4vV",
-	}
+	// prd6 := Product{
+	// 	ID: 6,
+	// 	Title: "Strawberry",
+	// 	Description: "Strawberries are sweet, juicy, and bursting with flavor.",
+	// 	Price: 500,
+	// 	ImgUrl: "https://snaped.fns.usda.gov/sites/default/files/styles/crop_ratio_7_5/public/seasonal-produce/2018-05/strawberries.jpg.webp?itok=B4LFd4vV",
+	// }
 
 	productList = append(productList, prd1)
 	productList = append(productList, prd2)
 	productList = append(productList, prd3)
-	productList = append(productList, prd4)
-	productList = append(productList, prd5)
-	productList = append(productList, prd6)
+	// productList = append(productList, prd4)
+	// productList = append(productList, prd5)
+	// productList = append(productList, prd6)
 
 }
-
-
